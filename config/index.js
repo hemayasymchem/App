111f2b56d9c2f046f0a4f07097f93118d1433d78
hemayasymchem <fengshimao@126.com>
2018-09-10 18:18:18 +0800

    demoCommit1.0

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'feng.erp.secoo.com', // can be overwritten by process.env.HOST
-    port: 9505, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 9589, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
